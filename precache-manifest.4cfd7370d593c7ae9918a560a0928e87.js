cf0822ae7dba46f9616fed85662b523d0136ada7
Shenyu <lishenyu16@gmail.com>
2019-05-10 17:19:15 -0700

    Updates

---
+++ precache-manifest.4cfd7370d593c7ae9918a560a0928e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf61bb7e61c644f4e5fccc161c9a4b7",
+    "url": "/react-stockmarket/index.html"
+  },
+  {
+    "revision": "cb1c6265336e8527cd29",
+    "url": "/react-stockmarket/static/css/main.9600569a.chunk.css"
+  },
+  {
+    "revision": "10cb45d70fe859b8c817",
+    "url": "/react-stockmarket/static/js/2.57e24081.chunk.js"
+  },
+  {
+    "revision": "cb1c6265336e8527cd29",
+    "url": "/react-stockmarket/static/js/main.bac56cc4.chunk.js"
+  },
+  {
+    "revision": "22d0c00d6434170eb888",
+    "url": "/react-stockmarket/static/js/runtime~main.a328085b.js"
+  },
+  {
+    "revision": "d362671e50e8543153b25f8f60cb5766",
+    "url": "/react-stockmarket/static/media/logo.d362671e.png"
+  }
+]);
